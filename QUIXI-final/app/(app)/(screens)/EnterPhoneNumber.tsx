19f85bba5bc9996cb0e9c8400648e59c5b4ae11f
Manarth Patel <manarthpatel237@gmail.com>
2024-03-18 19:45:43 -0400

    Updated by Manarth

--- QUIXI-final/app/(app)/(screens)/EnterPhoneNumber.tsx
+++ QUIXI-final/app/(app)/(screens)/EnterPhoneNumber.tsx
@@ -2,31 +2,10 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Button, SocialIcon, Text } from 'react-native-elements';
 
-export default function Signup() {
-
+export default function EnterPhoneNumber() {
   return (
-    <View style={styles.container}>
-      <Text h4>QUIXI</Text>
-      <Text h5>FIND YOUR SPARK WITH QUIXI</Text>
-      
-      <Button
-        title="Continue with email"
-        buttonStyle={styles.button}
-      />
-
-      <Button
-        title="Use phone number"
-        type="outline"
-        buttonStyle={styles.buttonOutline}
-      />
-
-      <View style={styles.socialContainer}>
-        <SocialIcon type='facebook' />
-        <SocialIcon type='google' />
-        <SocialIcon type='apple' />
-      </View>
-
-      <Text>Terms of use   Privacy Policy</Text>
+    <View>
+        <Text>Terms of use   Privacy Policy</Text>
     </View>
   );
 }
